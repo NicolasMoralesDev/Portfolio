f7146bfe66b3e0f5ea6c23ed8b1eb7501813c2d5
Nicolas <nicolasmoralesj03@gmail.com>
2022-12-08 02:13:01 -0300

    firts commit

---
+++ pages/sobremi.tsx
@@ -0,0 +1,15 @@
+import React, { Fragment, useState } from 'react'
+
+
+const [data, setData] = useState <String>();
+
+const sobremi = () => {
+  return (
+    <Fragment>
+    <div>sobremi</div>
+
+    </Fragment>
+  )
+}
+
+export default sobremi
