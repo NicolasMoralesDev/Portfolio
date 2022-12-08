c059cf8353ef7191e2600251c75ab8e4e498ff9e
Nicolas <nicolasmoralesj03@gmail.com>
2022-12-08 16:40:41 -0300

    se termina el footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
 
 const Footer = () => {
     return (
-        <footer className='w-full bg-stone-900	h-52 absolute bottom-0 text-white p-5'>
-
+        <footer className='w-full bg-stone-900	h-36 absolute bottom-0 text-white p-5'>
+            <ul className="flex flex-row justify-center gap-2 ">
+                <li><Link href="" className='flex gap-2'> <BsLinkedin/> Linkedin  </Link></li>
+                <li><Link href="" className='flex gap-2'> <BsGithub/>Github </Link></li>
+            </ul>
             <div className='w-full flex justify-center'>
-                <h2 className='absolute bottom-0 pb-3'>copyrith - Nicolas Morales</h2>
+                <h2 className='absolute bottom-0 pb-3'>© copyright - Nicolas Morales</h2>
 
             </div>
         </footer>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,11 +4,11 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className="bg-gray-800	text-white w-full h-15 p-3 flex ">
-      <ul className='flex w-full justify-center '>
-     <li className='pr-3'><Link href='/'>Home</Link></li> 
-       <li className='pr-3'> <Link href='/'>Contacto</Link></li>
-       <li className='pr-3'> <Link href='/'>Mis Proyectos</Link></li>
-  
+      <ul className='flex w-full justify-center gap-2 '>
+       <li><Link href='/'>Home</Link></li> 
+       <li> <Link href='/contacto'>Contacto</Link></li>
+       <li> <Link href='/proyectos'>Mis Proyectos</Link></li>
+
       </ul>
         
 
