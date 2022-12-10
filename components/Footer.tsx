c6fa6f6713f7731664c34d03dae7c4ba5a6e1e5e
Nicolas <nicolasmoralesj03@gmail.com>
2022-12-10 12:30:57 -0300

    se agrega medias querys

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,9 @@ import { BsGithub, BsLinkedin } from 'react-icons/bs';
 
 const Footer = () => {
     return (
-        <footer className='w-full bg-stone-900	h-36 relative bottom-0 text-white p-5'>
-            <ul className="flex flex-row justify-center gap-2 ">
-                <li><Link href="" className='flex gap-2'> <BsLinkedin/> Linkedin  </Link></li>
+        <footer className='w-full  bg-stone-900	h-36 relative bottom-0 text-white p-5'>
+            <ul className="flex max-md:flex-col max-md:items-center flex-row justify-center gap-2 ">
+                <li><Link href="https://www.linkedin.com/in/juannicolasmorales/" className='flex gap-2'> <BsLinkedin/> Linkedin  </Link></li>
                 <li><Link href="https://github.com/NicolasMoralesDev?tab=repositories"  className='flex gap-2'> <BsGithub/>Github </Link></li>
             </ul>
             <div className='w-full flex justify-center'>
