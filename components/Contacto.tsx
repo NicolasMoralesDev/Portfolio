c59f7b32c5586fc458ac33c989513ecd84e46543
Nicolas <nicolasmoralesj03@gmail.com>
2022-12-12 01:19:13 -0300

    se agrega fondo a seccion contactos

--- components/Contacto.tsx
+++ components/Contacto.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Contacto = () => {
   return (
-    <div className="flex p-5 justify-center flex-col items-center gap-4 w-full h-1/5">
-      <h2 className='text-4xl'>Contacto</h2>
-      <div className='flex items-center flex-col h-1/3 w-64 p-5  rounded-xl bg-red-800'>
+    <div className="flex p-5 contacto_container justify-center flex-col items-center gap-4 w-full h-1/5">
+      <h2 className='text-4xl text-white'>Dejame tu Mensaje</h2>
+      <div className='form_container flex items-center flex-col h-1/3 w-64 p-5  rounded-xl bg-red-800'>
         <form action="" method="post" className=' text-white flex items-center flex-col gap-4'>
           <label htmlFor="nombre">Nombre:
           <input type="text" name="nombre" id="nombre" placeholder='ingrese su nombre' required />
@@ -14,7 +14,7 @@ const Contacto = () => {
 
           </label>
           <label htmlFor="correo">Correo:
-          <input type="email" name="correo" id="correo" placeholder='ingrese su correo' required />
+          <input type="email" name="correo" id="correo" placeholder='ingrese su correo' />
           </label>
           <label htmlFor="mensaje">Mensaje:
           <textarea name="consulta" id="consulta" required placeholder='ingrese su mensaje' ></textarea>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 	text-white w-full h-16 p-5 flex ">
+    <nav className="bg-gray-800 sticky top-0 z-20	text-white w-full h-16 p-5 flex ">
       <ul className='flex w-full justify-center gap-2 '>
-        <li><Link  id='home' className='active:font-bold' href='/' >Home</Link></li> 
+        <li> <Link id='home' className='active:font-bold' href='/' >Home</Link></li> 
         <li> <Link id='/contacto'className='target:font-bold' href='/contacto'>Contacto</Link></li>
         <li> <Link id='/proyectos'className='target:font-bold' href='/proyectos'>Mis Proyectos</Link></li>
       </ul>
